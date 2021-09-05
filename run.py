dfcb9f31dbe5887f9b556085ae6d9936f0a2454d
GunaRakulan-GR <gunarakulan@gmail.com>
2021-09-06 00:13:02 +0530

    Create a folder for generated certificates

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import cv2
 with open('name-data.txt') as f:
    for line in f:
        img = cv2.imread("certificate-template.jpg")
-       cv2.imwrite("{}.jpg".format(line.strip()), img)
+       cv2.imwrite("generated-images/{}.jpg".format(line.strip()), img)
        print(line.strip())
 
 
