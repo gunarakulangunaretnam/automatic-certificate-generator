4479083dc5b9ba305f2324cb0b1e3c5e4bab7cb7
GunaRakulan-GR <gunarakulan@gmail.com>
2021-09-06 00:45:23 +0530

    Finish the basic prototype

--- run.py
+++ run.py
@@ -17,10 +17,11 @@ def cleanup_data():
 
 def generate_certificates():
 
-   for name in list_of_names:
+   for index, name in enumerate(list_of_names):
       certificate_template_image = cv2.imread("certificate-template.jpg")
       cv2.putText(certificate_template_image, name.strip(), (815,1500), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 0, 250), 5, cv2.LINE_AA)
       cv2.imwrite("generated-certificates/{}.jpg".format(name.strip()), certificate_template_image)
+      print("Processing {} / {}".format(index + 1,len(list_of_names)))
       
 
 
