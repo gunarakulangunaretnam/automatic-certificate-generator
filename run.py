f9d4b918d5f0042f68199eeeeb04dd9dec7e2e84
GunaRakulan-GR <gunarakulan@gmail.com>
2021-09-06 00:20:40 +0530

    Finish text writter

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ import cv2
 
 with open('name-data.txt') as f:
    for line in f:
-       img = cv2.imread("certificate-template.jpg")
-       cv2.imwrite("generated-images/{}.jpg".format(line.strip()), img)
+       certificate_template_image = cv2.imread("certificate-template.jpg")
+       cv2.putText(certificate_template_image, line.strip(), (815,1500), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 0, 250), 5, cv2.LINE_AA)
+       cv2.imwrite("generated-certificates/{}.jpg".format(line.strip()), certificate_template_image)
        print(line.strip())
 
 
